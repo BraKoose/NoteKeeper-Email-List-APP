cd1bd30d7b2ca30ba8c8be86a6e719166bedfb74
Koose <KooseTheBlogger@gmail.com>
2021-10-30 10:08:51 +0000

    Model Class'

--- app/src/main/java/com/example/notekeeper_of_email_name/MainActivity.kt
+++ app/src/main/java/com/example/notekeeper_of_email_name/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.notekeeper_of_email_name
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.annotation.SuppressLint
+
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = DatabaseHelper(this)
+
+        btn_Inser
     }
 }
---
+++ app/src/main/java/com/example/notekeeper_of_email_name/Model Class.kt
@@ -0,0 +1,9 @@
+package com.example.notekeeper_of_email_name
+
+import android.provider.ContactsContract
+
+class SqliteModel(
+    var id:Int? = 0,
+    var name: String? = null,
+    var email: String? =null
+)
